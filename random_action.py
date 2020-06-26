bc7e6caa0244f6b85deebf6b4453b6f7e62536d7
TzuYuOu <a1225johnny@gmail.com>
2020-06-26 11:15:26 +0800

    Choose random action

---
+++ random_action.py
@@ -0,0 +1,22 @@
+import gym
+
+if __name__ == "__main__":
+
+    env = gym.make('MountainCar-v0')
+    for i_episode in range(200):
+        
+        observation = env.reset() # reset environment to initial state for each episode
+        epi_rewards = 0 # accumulate rewards for each episode
+        
+        for t in range(250):
+            env.render()
+
+            action = env.action_space.sample() # choose a random action
+            observation, reward, done, info = env.step(action) # do the action, get the reward
+            epi_rewards += reward
+
+            if done:
+                print('Episode finished after {} timesteps, total rewards {}'.format(t+1, epi_rewards))
+                break
+
+    env.close() # need to close, or errors will be reported
